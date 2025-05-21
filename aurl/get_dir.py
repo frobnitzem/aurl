f72e4b775a267a294db4fb537d8ee9c22604932f
David M. Rogers <predictivestatmech@gmail.com>
2025-05-21 18:53:59 -0400

    Fixes from testing.

--- aurl/get_dir.py
+++ aurl/get_dir.py
@@ -2,7 +2,7 @@ __author__ = "David M. Rogers"
 __copyright__ = "UT-Battelle LLC"
 __license__ = "BSD3"
 
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 from pathlib import Path
 import asyncio
 import logging
@@ -18,11 +18,42 @@ from . import arun
 
 app = typer.Typer()
 
+async def get_list(url: str, M: Mirror, max_depth: int = 3,
+                   ignore_hidden: bool = True) -> List[URL]:
+    loc = await M.fetch(URL(f"{url}?max_depth={max_depth}"))
+    if loc is None:
+        print(f"Unable to download file listing for {url}")
+        sys.exit(1)
+    with loc.open() as f:
+        tree = json.load(f)
+
+    async def add_tree(path: str, t: Dict[str,Any], urls: List[URL]):
+        for name, entry in t.items():
+            if name.startswith(".") and ignore_hidden:
+                continue
+            rel = f"{path}/{name}"
+            tree = entry.get('children', False)
+            if tree:
+                if tree is True:
+                    loc = await M.fetch(f"{rel}?max_depth={max_depth}")
+                    if loc is None:
+                        print(f"Unable to download file listing for {rel}")
+                        continue
+                    with loc.open() as f:
+                        tree = json.load(f)
+                await add_tree(rel, tree, urls)
+                continue
+            urls.append(URL(rel))
+
+    urls: List[URL] = []
+    await add_tree(url, tree, urls)
+    return urls
+
 @app.command(help="Get a directory structure served by aurl.serve.")
-def get(url    : str = typer.Argument(..., help="directory tree root"),
-        mirror : Optional[Path] = typer.Option(None, help="directory holding downloaded files"),
-        v    : bool = typer.Option(False, "-v", help="show info-level logs"),
-        vv   : bool = typer.Option(False, "-vv", help="show debug-level logs")):
+def get_dir(url    : str = typer.Argument(..., help="directory tree root"),
+            mirror : Optional[Path] = typer.Option(None, help="directory holding downloaded files"),
+            v    : bool = typer.Option(False, "-v", help="show info-level logs"),
+            vv   : bool = typer.Option(False, "-vv", help="show debug-level logs")):
     if vv:
         logging.basicConfig(level=logging.DEBUG)
     elif v:
@@ -32,33 +63,8 @@ def get(url    : str = typer.Argument(..., help="directory tree root"),
 
     M = Mirror( mirror )
 
-    loc = arun(M.fetch(URL(f"{url}?max_depth=4")))
-    if loc is None:
-        print(f"Unable to download file listing for {url}")
-        sys.exit(1)
-    with loc.open() as f:
-        tree = json.load(f)
-
-    def add_tree(path, t, urls):
-        for entry in t:
-            name = f"{path}/{entry['name']}"
-            if entry.children:
-                if entry.children is True:
-                    loc = M.fetch(f"{name}?max_depth=4")
-                    if loc is None:
-                        print(f"Unable to download file listing for {name}")
-                        continue
-                    with loc.open() as f:
-                        tree = json.load(f)
-                    add_tree(name, tree, urls)
-                else:
-                    add_tree(name, entry.children, urls)
-                continue
-            urls.append(URL(name))
-
-    urls: List[URL] = []
-    add_tree(url, tree, urls)
-    paths = arun(M.fetch_all(urls))
+    urls = arun( get_list(url, M) )
+    paths = arun( M.fetch_all(urls) )
     print(json.dumps(dict((k.s, str(v)) for k, v in paths.items()), indent=4))
     sys.exit(0)
 
